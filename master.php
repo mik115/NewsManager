33aae49b363fdf78fb432c39b4b397ace4cb6419
michele <michele@michele-laptop.(none)>
2013-11-03 11:47:29 +0100

    aggiulta del modello master template aggiunta della libreria ckedit per l'edito

---
+++ master.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<script type="text/javascript" src='<?php echo $path ?>lib/jquery-1.10.2.min.js'></script>
+		<script type="text/javascript" src='<?php echo $path ?>lib/angular.min.js'></script>
+		<!--Header della pagina specifica-->
+		<?php echo head() ?>
+	</head>
+	<body>
+		<!--Content della pagina specifica-->
+		<?php echo content() ?>
+	</body>
+</head>

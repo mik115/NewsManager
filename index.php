b19a8a9894e3b97ed2d5158819f80523243b91fd
mik115 <miky881>
2013-11-05 20:54:04 +0100

    aggiunta login parte seconda

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
-<?php
-	//path che lo distanzia dalla master page
-	$path = "";
-	//user controll per l'header...
-	function head(){ ?>
-		
-		<title>Home Page</title>
-		<script type='text/javascript' src='script/prova.js'></script>
-		<script type="text/javascript" src="lib/ckeditor/ckeditor.js"></script>
-		<script type="text/javascript" src="lib/ckeditor/adapters/jquery.js"></script>
+<html>
+	<?php
+		if(isset($_COOKIE["PHPSESSID"])){
+			session_start();
+			if($_SESSION["scope"]=="NewsManager"){
+				header("Location: home.php");
+			}
+		}
+	?>
+	<head>
+		<script type="text/javascript" src='lib/jquery-1.10.2.min.js'></script>
 		<script type="text/javascript">
-			$(document).ready(function(){
-				$('#editor').ckeditor();
-			});
+			function login() {
+				if ($.trim($("input[name='username']").val())=="") {
+					alert("non hai inserito uno username");
+				}else if ($.trim($("input[name='passw']").val())=="") {
+					alert("non hai inserito una password");
+				}else{
+					$.ajax({
+						url: "PHP/login.php",
+						type: "post",
+						data: {uName: $("input[name='username']").val(), passw: $("input[name='passw']").val()}
+					}).done(function(result){
+						if (result) {
+							location.reload(true);
+						}else{
+							alert("username o password errata!");
+							$("input[name='passw']").val("");		
+						}
+					});
+				}
+			}
 		</script>
-	<?php }
-	
-	//user controll per il content..
-	function content(){ ?>
-	<div ng-app="phonecatApp">
-		<div ng-controller="PhoneListCtrl">
-			<label>Name:</label>
-			<input type="text" ng-model="yourName" placeholder="Enter a name here">
-			<hr>
-			<h1>Hello {{yourName}}!</h1>
-			
-			<ul>
-				<li class='phone' ng-repeat="phone in phones">
-				  <p class='phoneName'>{{phone.name}}</p>
-				  <p class='phoneCat'>{{phone.snippet}}</p>
-				</li>
-			</ul>
-		 </div>
-	</div>
-	<div>
-		<textarea id='editor'></textarea>
-	</div>
-	
-	<?php }
-
-	//chiamata alla masterPage
-	require("master.php");
-?>
+	</head>
+	<body>
+		<h1>
+			Login
+		</h1>
+		<p>Username: <input name=username type="text"/></p>
+		<p>Password: <input name="passw" type="password"/></p>
+		<input type="button" value="Login" onclick="login()"/>
+	</body>
+</head>

c20cd07ff8737189ff0ded5022a4135515ebfaa9
mik115 <miky881>
2013-11-05 20:52:46 +0100

    aggiunta della pagina e dei meccanismi di login

---
+++ PHP/login.php
@@ -0,0 +1,16 @@
+<?php
+	$dom = new DOMDocument();
+	$dom->load("../data/users.xml");
+	$xpath = new DOMXPath($dom);
+	$user = $xpath->query('/users/user[username="'.$_POST["uName"].'"]', $dom);
+	
+	if($user->length>0){
+		$passw=$user->item(0)->getElementsByTagName("password")->item(0)->nodeValue;
+		if($passw== sha1($_POST["passw"])){
+			session_start();
+			$_SESSION["user"]= $_POST["uName"];
+			$_SESSION["scope"]="NewsManager";
+			echo "OK";
+		}
+	}
+?>

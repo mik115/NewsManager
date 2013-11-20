12901a72afb80cce6f7aff9ed1fc46a50e519816
mpini <miky881@gmail.com>
2013-11-20 10:24:58 +0100

    commit di aggiornamento

---
+++ Template/InternalPageTemplate.php
@@ -0,0 +1,15 @@
+<?php
+	$path = "";
+	//user controll per l'header...
+	function head(){ ?>
+		<title>[PAGE TITLE]</title>
+	<?php }
+	
+	//user controll per il content..
+	function content(){ ?>
+	[CONTENT PAGE]
+	<?php }
+
+
+	require("master.php");
+?>

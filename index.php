d77e141d58e0a646211fb8b1fd6d4e0f18d001b7
mik115 <miky881>
2013-12-01 20:26:02 +0100

    tentativo di usare directive standard..riprovare..ti amo

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<!--librearia di ckeditor
 		<script type="text/javascript" src='lib/ckeditor/ckeditor.js'></script>
 		<script type="text/javascript" src="lib/ckeditor/adapters/jquery.js"></script>-->
-		<link rel='stylesheet' type='text/css' src='style/index.css'/>
+		<link rel='stylesheet' type='text/css' href='style/index.css'/>
 		<script type="text/javascript" src='script/index.js'></script>
 		<script type='text/javascript'>
 			angular.element(document).ready(function(){
@@ -15,20 +15,27 @@
 		//document.ready event
 			});
 		</script>
-		<style>
-			.redBorder{
-				border: 2px solid red;
-			}
-		</style>
 	<?php }
 	
 	//user controll per il content..
 	function content(){ ?>
+	
+	<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <a class="navbar-brand" >News Manager</a>
+		</div>
+	</nav>
+	
 	<div ng-app="mainApp">
 		<div ng-controller='auth'>
+			<div id='welcomeDiv'>
+				<h1>Benvenuto</h1>
+				<p>Per poter accedere alle funzionalità del News Manager devi prima effettuare il login.</p>
+			</div>
 			<p class='errorMessage' ng-show='loginError'>Username o Password Errati</p>
 			<p class='errorMessage' ng-show='emptyField'>Entrambi i campi sono required</p>
-			<p>Username: <input class='form-control' type='text' ng-model='username' required/></p>
+			<p>Username: <input keyPress="aaa()" class='form-control' type='text' ng-model='username' required /></p>
 			<p>Password: <input class='form-control' type='password' ng-model='password' required /></p>
 			<p><input class="btn btn-default" type='button' value='login' ng-click='login()' /></p>
 		</div>
--- script/index.js
+++ script/index.js
@@ -1,3 +1,6 @@
+//angular.module("main", [])
+	 
+
 
 //importante! va messo per la dichiarazione dell'app!
 var mainApp = angular.module('mainApp', []);
@@ -5,28 +8,49 @@ var mainApp = angular.module('mainApp', []);
 mainApp.controller("auth", function authCtrl($scope, $http){
 	 
 	 $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8;";
-	 
 	 $scope.login = function(){
-		if ($.trim($scope.username)=="" || $.trim($scope.password)=="") {
-		  $scope.emptyField=true;
-		}
-		else{
-		  $scope.emptyField=false;
-		  $http({
-			 url:"PHP/login.php",
-			 method: "POST",
-			 data : $.param({username : $scope.username, password: $scope.password})
-		 }).success(function(data, status, headers, config){
-			 if (data != "") {
-				//è andato tutto a buon fine!! sei loggato
-				window.location.href= "contents/home.php"
-			 }else{
-				 //non è andata bene
-				 $scope.loginError=true;
-			 }
-		 }).error(function(data, status, headers, config){
-			
-		 });
-		}
+		  if ($.trim($scope.username)=="" || $.trim($scope.password)=="") {
+			 $scope.emptyField=true;
+		  }
+		  else{
+				$scope.emptyField=false;
+				$http({
+					 url:"PHP/login.php",
+					 method: "POST",
+					 data : $.param({username : $scope.username, password: $scope.password})
+				}).success(function(data, status, headers, config){
+					 if (data != "") {
+						//è andato tutto a buon fine!! sei loggato
+						window.location.href= "contents/home.php"
+					 }else{
+						 //non è andata bene
+						 $scope.loginError=true;
+					 }
+				}).error(function(data, status, headers, config){
+				 
+				});
+		  }
 	 }
-  });
+	 
+	 $scope.aaa=function(){
+		  alert("aaa");
+	 }
+});
+
+mainApp.directive("keyPress", function(){
+		  return {
+	//			restrict: 'A',
+				link: function(scope, elem, attrs) {
+					  // this next line will convert the string
+					  // function name into an actual function
+					  var functionToCall = scope.$eval(attrs.keyPress);
+					  elem.on('keydown', function(e){
+							 // on the keydown event, call my function
+							 // and pass it the keycode of the key
+							 // that was pressed
+							 // ex: if ENTER was pressed, e.which == 13
+							 functionToCall(e.which);
+					  });
+				}
+		  };
+	 });

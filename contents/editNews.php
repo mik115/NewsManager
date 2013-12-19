264ae7d555aa6b545d15e13f1da619f2d0d49d87
mpini <miky881@gmail.com>
2013-12-19 13:23:02 +0100

    Sostituiti i le multiselect con le select di bootstrap

--- contents/editNews.php
+++ contents/editNews.php
@@ -3,26 +3,17 @@
 	//user controll per l'header...
 	function head($path){ ?>
 		<title ng-controller='pageTitleSetter'>{{pageTitle}}</title>
+		<link rel="stylesheet" type='text/css' href='<?php echo $path?>lib/bootstrap-select/bootstrap-select.css'/>
 		<link rel="stylesheet" type='text/css' href='<?php echo $path?>style/editNews.css'/>
-		<link rel="stylesheet" type='text/css' href='<?php echo $path?>lib/bootstrap-multiselect/css/bootstrap-multiselect.css'/>
 		<script type="text/javascript" src='<?php echo $path?>lib/ckeditor/ckeditor.js'></script>
-		<script type= "text/javascript" src = '<?php echo $path?>lib/bootstrap-multiselect/js/bootstrap-multiselect.js'></script>
+		<script type= "text/javascript" src = '<?php echo $path?>lib/bootstrap-select/bootstrap-select.js'></script>
 		<script type='text/javascript' src='<?php echo $path?>script/editNews.js'></script>
 		<script type='text/javascript'>
 			angular.element(document).ready(function(){
 				CKEDITOR.replace( "textEditor",{
 					uiColor: '#F4F4F4'
 				});
-				$("#tagsSelection").multiselect({
-					enableCaseInsensitiveFiltering: true,
-					maxHeight: "50px",
-					buttonWidth: "200px"
-				});
-				
-				$("#categoryButton").multiselect({
-					enableCaseInsensitiveFiltering: true,
-					buttonWidth: "200px"
-				});
+				$('.selectpicker').selectpicker();
 			});
 		</script>
 	<?php }
@@ -35,17 +26,41 @@
 				<div class='leftDiv'>
 					<p>Titolo: <input class='form-control' value='titolo'/></p>
 					<p>Data Pubblicazione: <input class='form-control' value='DataPubblicazione {timePicker}'/></p>
-					<select id='tagsSelection' multiple="multiple">
-					<!--qui ci vanno i possibili tags-->
-					<option>i possibili tag</option>
+					<select id='tagsSelection' class='selectpicker' multiple data-live-search="true"
+    						  title='Scegli dei tag' data-selected-text-format="count>2" data-count-selected-text="{0} selected" data-size=10>
+						<!--qui ci vanno i possibili tags-->
+						<option>i possibili tag</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+						<option>altre opzioni</option>
+					</select>
 				</select>
 				</div>
 				<div class='rightDiv'>
 					<p>Sottotitolo: <input class='form-control' value='sottotitolo'/></p>
 					<p>Importante: <input class='form-control' value='importante {checkbox}'/></p>
-					<select id='categoryButton'>
-					<option>Una categoria</option>
-				</select>
+					<select id='categoryButton' class='selectpicker show-tick' data-live-search="true" title='Scegli una categoria' data-size=10>
+    					<option>Una categoria</option>
+						<option>un'altra categoria</option>
+						<option>un'altra categoria</option>
+						<option>un'altra categoria</option>
+						<option>un'altra categoria</option>
+						<option>un'altra categoria</option>
+					</select>
 				</div>
 			</div>
 			<textarea id='textEditor'>{{newsBody}}</textarea>

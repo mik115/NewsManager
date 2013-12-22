3caf73ac0fb092d0ac4ab5916be0a0ae6f398ce2
mpini <miky881@gmail.com>
2013-12-22 02:44:22 +0100

    aggiunto il dateTime picker alla creazione / modificha delle news.

--- contents/editNews.php
+++ contents/editNews.php
@@ -4,16 +4,25 @@
 	function head($path){ ?>
 		<title ng-controller='pageTitleSetter'>{{pageTitle}}</title>
 		<link rel="stylesheet" type='text/css' href='<?php echo $path?>lib/bootstrap-select/bootstrap-select.css'/>
+		<link rel="stylesheet" type='text/css' href='<?php echo $path?>lib/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css'/>
 		<link rel="stylesheet" type='text/css' href='<?php echo $path?>style/editNews.css'/>
 		<script type="text/javascript" src='<?php echo $path?>lib/ckeditor/ckeditor.js'></script>
 		<script type= "text/javascript" src = '<?php echo $path?>lib/bootstrap-select/bootstrap-select.js'></script>
+		<script type= "text/javascript" src = '<?php echo $path?>lib/moment/moment.js'></script>
+		<script type= "text/javascript" src = '<?php echo $path?>lib/bootstrap-datetimepicker/src/js/locales/bootstrap-datetimepicker.it.js'></script>
+		<script type= "text/javascript" src = '<?php echo $path?>lib/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'></script>
 		<script type='text/javascript' src='<?php echo $path?>script/editNews.js'></script>
 		<script type='text/javascript'>
 			angular.element(document).ready(function(){
 				CKEDITOR.replace( "textEditor",{
 					uiColor: '#F4F4F4'
 				});
-				$('.selectpicker').selectpicker();
+				$('.selectpicker').selectpicker({
+					width: "100%"
+				});
+				$('#datetimepicker1').datetimepicker({
+					language: 'it'
+				});
 			});
 		</script>
 	<?php }
@@ -24,8 +33,18 @@
 			<input id='backButton' type='button' class='btn btn-default' value='Anunlla'/>
 			<div id='metaContent'>
 				<div class='leftDiv'>
-					<p>Titolo: <input class='form-control' value='titolo'/></p>
-					<p>Data Pubblicazione: <input class='form-control' value='DataPubblicazione {timePicker}'/></p>
+					<div class='formRow'>Titolo: <input class='form-control' value='titolo'/></div>
+					<div class='formRow'>
+						Data Pubblicazione:
+						<div class="form-group">
+							<div class='input-group date' id='datetimepicker1'>
+								<input type='text' class="form-control" data-format="ddd DD-MM-YYYY HH:mm" />
+								<span class="input-group-addon">
+									<span class="glyphicon glyphicon-calendar"></span>
+								</span>
+							</div>
+						</div>
+					</div>
 					<select id='tagsSelection' class='selectpicker' multiple data-live-search="true"
     						  title='Scegli dei tag' data-selected-text-format="count>2" data-count-selected-text="{0} selected" data-size=10>
 						<!--qui ci vanno i possibili tags-->
@@ -48,11 +67,10 @@
 						<option>altre opzioni</option>
 						<option>altre opzioni</option>
 					</select>
-				</select>
 				</div>
 				<div class='rightDiv'>
-					<p>Sottotitolo: <input class='form-control' value='sottotitolo'/></p>
-					<p>Importante: <input class='form-control' value='importante {checkbox}'/></p>
+					<div class='formRow'>Sottotitolo: <input class='form-control' value='sottotitolo'/></div>
+					<div class='formRow'>Importante: <input class='form-control' value='importante {checkbox}'/></div>
 					<select id='categoryButton' class='selectpicker show-tick' data-live-search="true" title='Scegli una categoria' data-size=10>
     					<option>Una categoria</option>
 						<option>un'altra categoria</option>

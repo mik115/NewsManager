7a93c306136f1d2cd2929b0bc0689e21fc9964b8
mpini <miky881@gmail.com>
2014-07-02 14:06:31 +0200

    inizio implementazione visualizzazione categorie presenti nel sistema. #25

--- contents/Settings/Categorie/index.php
+++ contents/Settings/Categorie/index.php
@@ -12,7 +12,33 @@
 		<div ng-controller="categoryController">
 			<h1>Gestisci le Categorie</h1>
 			<p>Crea nuove categorie, modifica quelle esistenti o eliminale</p>
+			<p class="filterRow">Filtra: <input type="text" id="searchField" ng-model="search" class="form-control" /></p>
+			<input type="button" class="btn btn-primary addTagButton" value="Aggiungi" ng-click="add();"/>
+			<table class="table table-striped table-responsive">
+				<tr>
+					<th ng-click="orderable='nome'; reverse=!reverse" class="orderable">Tag Name
+						<span ng-show="orderable=='nome' && !reverse" class="glyphicon glyphicon-chevron-up orderVersus"></span>
+						<span ng-show="orderable=='nome' && reverse" class="glyphicon glyphicon-chevron-down orderVersus"></span>
+					</th>
+					<th ng-click="orderable='ricorrenze'; reverse=!reverse" class="orderable">Ricorrenze
+						<span ng-show="orderable=='ricorrenze' && !reverse" class="glyphicon glyphicon-chevron-up orderVersus"></span>
+						<span ng-show="orderable=='ricorrenze' && reverse" class="glyphicon glyphicon-chevron-down orderVersus"></span>
+					</th>
+					<th></th>
+					<th></th>
+				</tr>
+				<tr ng-repeat="cat in categories | filter: searchFunction | orderBy: orderable: reverse">
+					<td>{{cat.nome}}</td>
+					<td>{{cat.ricorrenze}}</td>
+					<td class='buttonCell'><input type= "button" value="Edit"  class="btn btn-success" ng-click="edit(cat.id);" /></td>
+					<td class='buttonCell'><input type= "button" value="Delete" class="btn btn-danger" ng-click="del(cat.id, categories.indexOf(cat));" /></td>
+				</tr>
+			</table>
+		
 		</div>
+		
+		
+		
 	<?php }
 
 
--- script/settings/catogorie.js
+++ script/settings/catogorie.js
@@ -1,6 +1,44 @@
 
-mainModule.controller('categoryController', function impMainCtrl($scope, classPage){
+mainModule.controller('categoryController', function impMainCtrl($scope, classPage, $http){
 	classPage.setClassPage("Impostazioni"); //per evidenziare il link corrente
+	
+	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8;";
+
+	$http({
+		url: pagePath+"PHP/newsHandler.php",
+		method: "POST",
+		data : $.param({
+			action: "GetAllCategories"
+		})
+	}).success(function(data, status, headers, config){
+		if (data != false) {
+			$http({
+				url: pagePath+"PHP/newsHandler.php",
+				method: "POST",
+				data : $.param({
+					action: "GetAllNews"
+				})
+			}).success(function(result){
+				$scope.categories = data;
+				
+				for (var t in $scope.categories) {
+					$scope.categories[t].ricorrenze = 0;
+					for (var i in result){
+						if (result[i].categoria == $scope.categories[t].id){
+							$scope.categories[t].ricorrenze++;
+						}
+					}
+				}
+			});
+		}else{
+			//non è andata bene
+			//TODO mostrare un messaggio di errore sul caricamento della news all'interno della pagina.
+		}
+	}).error(function(data, status, headers, config){
+		//non è andata bene
+		//TODO mostrare un messaggio di errore sul caricamento della news all'interno della pagina.
+	});
+	
 	
 	
 });
